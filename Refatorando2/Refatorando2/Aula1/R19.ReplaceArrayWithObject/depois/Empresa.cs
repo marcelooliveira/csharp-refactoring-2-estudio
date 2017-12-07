ecbfcdac8f63d06e0cbe804a8250f57d02753e56
RafaelNercessian <mclricardo@gmail.com>
2017-12-07 15:24:49 -0200

    .
    .

--- Refatorando2/Refatorando2/Aula1/R19.ReplaceArrayWithObject/depois/Empresa.cs
+++ Refatorando2/Refatorando2/Aula1/R19.ReplaceArrayWithObject/depois/Empresa.cs
@@ -14,66 +14,16 @@ namespace refatoracao.R19.ReplaceArrayWithObject.depois
                 string linha = string.Empty;
                 while ((linha = streamReader.ReadLine()) != null)
                 {
-                    string[] campos = linha.Split(',');
-                    var cliente = new Cliente(
-                        campos[0], campos[1], campos[2], campos[3]);
+                    string[] cliente = linha.Split(',');
 
                     Console.WriteLine("Dados do Cliente");
                     Console.WriteLine("================");
-                    Console.WriteLine("ID: " + cliente.Id);
-                    Console.WriteLine("Nome: " + cliente.Nome);
-                    Console.WriteLine("Telefone: " + cliente.Fone);
-                    Console.WriteLine("Website: " + cliente.Website);
+                    Console.WriteLine("ID: " + cliente[0]);
+                    Console.WriteLine("Nome: " + cliente[1]);
+                    Console.WriteLine("Telefone: " + cliente[2]);
+                    Console.WriteLine("Website: " + cliente[3]);
                     Console.WriteLine("================");
                 }
-            }
-        }
-    }
-
-    internal class Cliente
-    {
-        readonly string id;
-        readonly string nome;
-        readonly string fone;
-        readonly string website;
-
-        public Cliente(string id, string nome, string fone, string website)
-        {
-            this.id = id;
-            this.nome = nome;
-            this.fone = fone;
-            this.website = website;
-        }
-
-        public string Id
-        {
-            get
-            {
-                return id;
-            }
-        }
-
-        public string Nome
-        {
-            get
-            {
-                return nome;
-            }
-        }
-
-        public string Fone
-        {
-            get
-            {
-                return fone;
-            }
-        }
-
-        public string Website
-        {
-            get
-            {
-                return website;
             }
         }
     }
