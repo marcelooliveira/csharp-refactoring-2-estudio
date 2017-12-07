01d7cef060b28e300023c69dc0da304d2c3fb508
RafaelNercessian <mclricardo@gmail.com>
2017-12-07 15:37:56 -0200

    744-csharprefatorandocodigo2-video1.3-Substituir Array Por Objeto
    744-csharprefatorandocodigo2-video1.3-Substituir Array Por Objeto

--- Refatorando2/Refatorando2/Aula1/R19.ReplaceArrayWithObject/depois/Empresa.cs
+++ Refatorando2/Refatorando2/Aula1/R19.ReplaceArrayWithObject/depois/Empresa.cs
@@ -14,17 +14,44 @@ namespace refatoracao.R19.ReplaceArrayWithObject.depois
                 string linha = string.Empty;
                 while ((linha = streamReader.ReadLine()) != null)
                 {
-                    string[] cliente = linha.Split(',');
+                    string[] campos = linha.Split(',');
+
+                    Cliente cliente = new Cliente(int.Parse(campos[0]), campos[1], campos[2], campos[3]);
 
                     Console.WriteLine("Dados do Cliente");
                     Console.WriteLine("================");
-                    Console.WriteLine("ID: " + cliente[0]);
-                    Console.WriteLine("Nome: " + cliente[1]);
-                    Console.WriteLine("Telefone: " + cliente[2]);
-                    Console.WriteLine("Website: " + cliente[3]);
+                    Console.WriteLine("ID: " + cliente.Id);
+                    Console.WriteLine("Nome: " + cliente.Nome);
+                    Console.WriteLine("Telefone: " + cliente.Telefone);
+                    Console.WriteLine("Website: " + cliente.Website);
                     Console.WriteLine("================");
                 }
             }
         }
+    }
+
+    class Cliente
+    {
+        readonly int id;
+        readonly string nome;
+        readonly string telefone;
+        readonly string website;
+
+        public int Id => id;
+
+        public string Nome => nome;
+
+        public string Telefone => telefone;
+
+        public string Website => website;
+
+        public Cliente(int id, string nome, string telefone, string website)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.telefone = telefone;
+            this.website = website;
+        }
+
     }
 }
